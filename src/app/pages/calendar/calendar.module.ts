de8c2c2fb20e21c064917f559ba3a5a1160716f2
Chugaev Sergey <elov4anin@gmail.com>
2020-02-17 12:02:25 +0500

    page calendar - calendar

--- src/app/pages/calendar/calendar.module.ts
+++ src/app/pages/calendar/calendar.module.ts
@@ -4,11 +4,13 @@ import { CalendarPageRoutingModule } from './calendar-routing.module';
 
 import { CalendarPage } from './calendar.page';
 import {SharedModule} from "../../shared/shared.module";
+import {NgCalendarModule} from "ionic2-calendar";
 
 @NgModule({
   imports: [
     SharedModule,
-    CalendarPageRoutingModule
+    CalendarPageRoutingModule,
+    NgCalendarModule
   ],
   declarations: [CalendarPage]
 })
