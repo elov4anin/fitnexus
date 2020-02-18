246decad6efd4d0682913d499a74e23c2c73a3ff
Chugaev Sergey <elov4anin@gmail.com>
2020-02-18 21:41:02 +0500

    page account -  inputs

--- src/app/pages/account/account.page.ts
+++ src/app/pages/account/account.page.ts
@@ -14,4 +14,7 @@ export class AccountPage implements OnInit {
     ngOnInit() {
     }
 
+    saveProfile() {
+
+    }
 }
--- src/app/shared/layouts/account-layout/account-layout.component.ts
+++ src/app/shared/layouts/account-layout/account-layout.component.ts
@@ -1,14 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {ICommon} from "../../interfaces/common.interfaces";
+import ITopButton = ICommon.ITopButton;
 
 @Component({
-  selector: 'app-account-layout',
-  templateUrl: './account-layout.component.html',
-  styleUrls: ['./account-layout.component.scss'],
+    selector: 'app-account-layout',
+    templateUrl: './account-layout.component.html',
+    styleUrls: ['./account-layout.component.scss'],
 })
 export class AccountLayoutComponent implements OnInit {
+    buttons: ITopButton[] = [
+        {
+            text: 'trackers',
+            icon: 'ios-person-black.svg',
+            redirectTo: 'stat-by-device',
+            active: true
+        },
+        {
+            text: 'fitness',
+            icon: 'ios-fitness.svg',
+            redirectTo: 'welcome-programs',
+            active: false
+        },
+        {
+            text: 'body-comp',
+            icon: 'ios-photos-black.svg',
+            redirectTo: 'add-body-comp',
+            active: false
+        }
+    ];
 
-  constructor() { }
+    constructor(private _router: Router) {
+    }
 
-  ngOnInit() {}
+    ngOnInit() {
+    }
 
+    toggleButton(redirectTo: string) {
+        const idx = this.buttons.findIndex(b => b.redirectTo === redirectTo);
+        this.buttons.forEach(b => b.active = false);
+        this.buttons[idx] = {
+            ...this.buttons[idx],
+            active: true
+        };
+        this._router.navigate(["/", redirectTo]);
+    }
 }
