93fa16ecc5533f16670187529dfc25ab430227b8
Chugaev Sergey <elov4anin@gmail.com>
2020-02-19 16:15:11 +0500

    page messages and chat

---
+++ src/app/pages/chat/chat.page.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-chat',
+    templateUrl: './chat.page.html',
+    styleUrls: ['./chat.page.scss'],
+})
+export class ChatPage implements OnInit {
+    pageTitle: string = '';
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    addComment() {
+
+    }
+}
