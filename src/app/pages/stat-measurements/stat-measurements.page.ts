2b9ada32933af4b58d98c00e72a4c4c5c3393c16
Chugaev Sergey <elov4anin@gmail.com>
2020-03-05 22:16:02 +0500

    FIX stat-measurements

--- src/app/pages/stat-measurements/stat-measurements.page.ts
+++ src/app/pages/stat-measurements/stat-measurements.page.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {PageBaseComponent} from "../../shared/components/page-base/page-base.component";
+import {CalendarComponentOptions, CalendarModal, CalendarModalOptions} from "ion2-calendar";
+import * as moment from "moment";
+import {ModalController} from "@ionic/angular";
 
 @Component({
     selector: 'app-stat-measurements',
@@ -9,8 +12,14 @@ import {PageBaseComponent} from "../../shared/components/page-base/page-base.com
 })
 export class StatMeasurementsPage extends PageBaseComponent implements OnInit {
     pageTitle: string = 'TRACKING - BODY COMP';
+    selectedDate: any;
+    type: 'string'; // 'string' | 'js-date' | 'moment' | 'time' | 'object'
+    optionsRange: CalendarComponentOptions = {
+        pickMode: 'range'
+    };
+    dateLabel: string = 'Select date';
 
-    constructor(private _router: Router) {
+    constructor(private _router: Router, public modalCtrl: ModalController) {
         super();
     }
 
@@ -22,7 +31,25 @@ export class StatMeasurementsPage extends PageBaseComponent implements OnInit {
     }
 
     openMeasurement() {
-      this._router.navigate(['/measurement-detail'])
+      this._router.navigate(['/measurement-detail']);
+    }
 
+    async openCalendar() {
+        const options: CalendarModalOptions = {
+            title: 'Select date'
+        };
+
+        const myCalendar = await this.modalCtrl.create({
+            component: CalendarModal,
+            componentProps: { options }
+        });
+
+        myCalendar.present();
+
+        const event: any = await myCalendar.onDidDismiss();
+        const date = event.data;
+        this.selectedDate = date;
+        this.dateLabel =  moment([date.years, date.months, date.date]).format('DD/MM/YYYY');
+        console.log(date);
     }
 }
