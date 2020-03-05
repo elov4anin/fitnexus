ab640022a683f8654e7fddeea76f37f4313e3895
Chugaev Sergey <elov4anin@gmail.com>
2020-03-05 20:15:08 +0500

    FIX text in date dropdown and steps dropdown should be black and the arrows should be blue FIX text under the number 3221 on the page showing the data for the delected date and spacing not consistent with design

--- src/app/pages/stat-by-device/stat-by-device.page.ts
+++ src/app/pages/stat-by-device/stat-by-device.page.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {PageBaseComponent} from "../../shared/components/page-base/page-base.component";
+import {IonSelect} from "@ionic/angular";
 
 @Component({
     selector: 'app-stat-by-device',
@@ -8,12 +9,27 @@ import {PageBaseComponent} from "../../shared/components/page-base/page-base.com
 })
 export class StatByDevicePage extends PageBaseComponent implements OnInit {
     pageTitle: string = 'Tracking - Wearables & Apps';
+    selectedDevice: any;
+    @ViewChildren('ion-select') selectsRef: QueryList<IonSelect>;
+    selectedStep: any;
+    selectedDateRange: any;
+
 
     constructor() {
         super();
     }
 
     ngOnInit() {
+    }
+
+    ionViewDidEnter() {
+        const ionSelects = document.querySelectorAll('ion-select');
+        ionSelects.forEach((ionSelect) => {
+            const selectIconInner = ionSelect.shadowRoot.querySelector('.select-icon').querySelector('.select-icon-inner');
+            if(selectIconInner){
+                selectIconInner.setAttribute('style', 'display: none !important');
+            }
+        });
     }
 
     addDevice() {
